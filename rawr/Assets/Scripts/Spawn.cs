d646daaaf20e2dbff699a86683a2076af56fc4f3
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2024-07-11 23:20:12 +0200

    cam and meteor

--- rawr/Assets/Scripts/Spawn.cs
+++ rawr/Assets/Scripts/Spawn.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -7,19 +8,28 @@ using Random = UnityEngine.Random;
 
 public class Spawn : MonoBehaviour
 {
-    static public KeyCode spawnKey = KeyCode.F;
+    static public KeyCode spawnKey = KeyCode.Space;
     public GameObject meteor;
+    public Transform planet;
+
+    private void Start()
+    {
+        planet = GameObject.FindWithTag("Ground").transform;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(spawnKey))
+        if (Input.GetKeyDown(spawnKey) || Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log(Camera.main.transform.up);
-            Vector2 spawnPos = Camera.main.transform.position;
-            spawnPos += (Vector2)(Camera.main.transform.up * 5);
-            spawnPos += (Vector2)(Camera.main.transform.right *
-                                  Random.Range(-8, 8));
+            if ((Camera.main.ScreenToWorldPoint(Input.mousePosition) - planet.position).magnitude > 27f)
+                return;
+            
+            transform.up = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)planet.position).normalized;
+            Vector3 spawnPos = planet.position;
+            spawnPos += (transform.up * 28);
+            spawnPos += (transform.right * Random.Range(-8, 8));
+            
             GameObject obj = Instantiate(meteor, spawnPos,quaternion.identity);
             obj.GetComponent<Meteor>().SetInitialVelocity();
         }
