88154161bce072dc7722e45e94d781d1d24a3906
kellenstrydom <131270586+kellenstrydom@users.noreply.github.com>
2024-07-12 14:48:04 +0200

    chance walking

---
+++ rawr/Assets/Scripts/ColourController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourController : MonoBehaviour
+{
+    public List<Transform> allBuildings;
+    public List<Transform> allPeople;
+
+    public Color colour;
+}
--- rawr/Assets/Scripts/PersonMovement.cs
+++ rawr/Assets/Scripts/PersonMovement.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SubsystemsImplementation;
+using Random = UnityEngine.Random;
 
 public class PersonMovement : MonoBehaviour
 {
@@ -8,51 +14,91 @@ public class PersonMovement : MonoBehaviour
         left,
         right
     }
+
+    [Serializable]
+    struct probalities
+    {
+        public float left;
+        public float right;
+        public float still;
+    }
     
     public Transform circle; 
     public float speed = 30.0f;
     public float changeDirectionInterval = 2.0f;
     public State currentState;
-    
-    
+    public ColourController _colourController;
 
+    public AnimationCurve curve;
+
+    public float stillChance;
+
+    public float breedChance;
+    
+    private bool canBreed = false;
+    
     private float radius;
     private float angle; 
     
     private bool moveClockwise; 
     private float nextDirectionChangeTime; 
 
-    void Start()
+    void Awake()
     {
+        circle = GameObject.FindWithTag("Ground").transform;
+            
         if (circle == null)
         {
             Debug.LogError("Circle transform is not assigned.");
             return;
         }
 
+        if (_colourController == null)
+        {
+            Debug.Log("No colour controller");
+            return;
+        }
+        _colourController.allPeople.Add(transform);
+        GetComponent<SpriteRenderer>().color = _colourController.colour;
+        
+        StartCoroutine(Tick(changeDirectionInterval));
+        
         radius = Vector3.Distance(transform.position, circle.position);
 
         Vector3 direction = transform.position - circle.position;
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        moveClockwise = Random.value < 0.5f;
-
         UpdatePositionAndRotation();
 
-        nextDirectionChangeTime = Time.time + changeDirectionInterval;
+        StartCoroutine(BreedTimer(5f));
+        //moveClockwise = Random.value < 0.5f;
+
+
+
+        //nextDirectionChangeTime = Time.time + changeDirectionInterval;
     }
 
     void Update()
     {
-        if (Time.time >= nextDirectionChangeTime)
+        // if (Time.time >= nextDirectionChangeTime)
+        // {
+        //     float randomAngle = Random.Range(0.0f, 360.0f);
+        //     moveClockwise = angle < randomAngle;
+        //
+        //     nextDirectionChangeTime = Time.time + changeDirectionInterval;
+        // }
+
+        switch (currentState)
         {
-            float randomAngle = Random.Range(0.0f, 360.0f);
-            moveClockwise = angle < randomAngle;
-
-            nextDirectionChangeTime = Time.time + changeDirectionInterval;
+            case State.right:
+                angle -= speed * Time.deltaTime;
+                break;
+            case State.left:
+                angle += speed * Time.deltaTime;
+                break;
+            case State.still:
+                break;
         }
-
-        angle += moveClockwise ? speed * Time.deltaTime : -speed * Time.deltaTime;
+        //angle += moveClockwise ? speed * Time.deltaTime : -speed * Time.deltaTime;
 
         if (angle >= 360.0f)
         {
@@ -66,14 +112,134 @@ public class PersonMovement : MonoBehaviour
         UpdatePositionAndRotation();
     }
 
+    Transform ClosestBuilding()
+    {
+        Transform closestBuilding = null;
+        foreach (Transform building in _colourController.allBuildings)
+        {
+            if (closestBuilding == null)
+            {
+                closestBuilding = building;
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position,building.position) < Vector3.Distance(transform.position,closestBuilding.position))
+                {
+                    closestBuilding = building;
+                }
+            }
+        }
+
+        return closestBuilding;
+    }
+
     void UpdatePositionAndRotation()
     {
         float x = circle.position.x + Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
         float y = circle.position.y + Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
 
         transform.position = new Vector3(x, y, transform.position.z);
+        
 
-        float rotationAngle = angle + 90.0f; 
+        float rotationAngle = angle - 90.0f; 
         transform.rotation = Quaternion.Euler(0, 0, rotationAngle);
+    }
+
+    IEnumerator Tick(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+
+        Transform closestBuilding = ClosestBuilding();
+        
+        Vector3 startDir = transform.position.normalized;
+        Vector3 targetDir = closestBuilding.position.normalized;
+        float angleFromBuilding = Vector3.Angle(startDir, targetDir);
+        Vector3 cross = Vector3.Cross(startDir, targetDir);
+        
+        //Debug.Log($"angle : {angleFromBuilding}");
+        //Debug.Log($"cross : {cross}");
+
+        float rightChance;
+        float leftChance;
+        if (cross.z > 0) // Assuming positive y-axis is the "up" direction
+        {
+
+            rightChance = curve.Evaluate(angleFromBuilding / 180) * (.5f - stillChance / 2) ;
+            //rightChance = Mathf.Lerp(.5f - stillChance / 2, minBravery, angleFromBuilding / 180);
+            leftChance = 1 - stillChance - rightChance;
+            //Debug.Log("right");
+            
+        }
+        else
+        {
+            leftChance = curve.Evaluate(angleFromBuilding / 180)  * (.5f - stillChance / 2);
+            //leftChance = Mathf.Lerp(.5f - stillChance / 2, minBravery, angleFromBuilding / 180);
+            rightChance = 1 - stillChance - leftChance;
+            //Debug.Log("left");
+            
+        }
+        
+        float randValue = Random.value;
+        if (randValue < stillChance)
+        {
+            currentState = State.still;
+        }
+        else if (randValue < (stillChance + leftChance)) // 45% of the time
+        {
+            currentState = State.left;
+        }
+        else // 10% of the time
+        {
+            currentState = State.right;
+        }
+
+        //Debug.Log($"left:{leftChance}, right:{rightChance}, still:{stillChance} = {leftChance + rightChance + stillChance}");
+
+        float tickVariance = Random.Range(-.5f, .5f);
+        StartCoroutine(Tick(changeDirectionInterval + tickVariance));
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Explosion"))
+        {
+            Die();
+        }
+        
+        if (!col.CompareTag("people")) return;
+        
+        //Debug.Log("Collided with: " + col.gameObject.name);
+        PersonMovement otherPerson = col.transform.GetComponent<PersonMovement>();
+
+        if (otherPerson == null)
+        {
+            Debug.LogWarning("ColourController component not found on the collided object.");
+            return;
+        }
+        
+        if (!canBreed) return;
+        if (_colourController == otherPerson._colourController) // same colour
+        {
+            //Debug.Log("hitt");
+            float randValue = Random.value;
+            if (randValue < breedChance)
+            {
+                Debug.Log("Breeeeeeed");
+                Instantiate(gameObject, transform.position, quaternion.identity);
+                StartCoroutine(BreedTimer(5f));
+            }
+        }
+    }
+
+    IEnumerator BreedTimer(float wait)
+    {
+        canBreed = false;
+        yield return new WaitForSeconds(wait);
+        canBreed = true;
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
